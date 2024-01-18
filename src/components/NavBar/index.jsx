e3924213142d72bdcb18b10bcf18678d94aa9e78
Ewelina Sobora <e@e-dot.uk>
2024-01-18 15:55:32 +0000

    add hamburger menu icon

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,10 +1,13 @@
 import './style.css'
 import { useState } from 'react'
-import { Navbar, Container } from 'react-bootstrap'
+import { Navbar, Container, Nav, Dropdown } from 'react-bootstrap'
 import { Twirl as Hamburger } from 'hamburger-react'
 import logo from '/assets/images/qc-logo.jpg'
+import sections from '../../data/sections.json'
 
 function NavBar() {
+  const [isOpen, setOpen] = useState(false);
+
   return (
     <div className="navbar-container">
       <Navbar className="bg-dark-subtle">
@@ -16,12 +19,31 @@ function NavBar() {
               width="180"
               height="80"
               className="d-inline-block align-top"
-            />{' '}
+            />
           </Navbar.Brand>
+          <Hamburger toggled={isOpen} toggle={setOpen} />
+          <Dropdown className={`navbar-menu ${isOpen ? 'active' : ''}`}>
+            {isOpen && (
+              <ul className="grid gap-2">
+                {sections.map((section) => (
+                  <li
+                    key={section.title}
+                    className="w-full p-[0.08rem] rounded-xl bg-gradient-to-tr from-neutral-800 via-neutral-950 to-neutral-700">
+                    <a
+                      onClick={() => setOpen(false)}
+                      className="flex items-center justify-between w-full p-5 rounded-xl bg-neutral-950"
+                      href={section.href}>
+                      <span className="flex gap-1 text-lg">{section.title}</span>
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            )}
+          </Dropdown>
         </Container>
       </Navbar>
     </div>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;

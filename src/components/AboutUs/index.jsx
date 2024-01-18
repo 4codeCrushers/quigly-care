140a1aab8a6508052822e957700a4f3b08eb3f49
Dom B <dominicbrown4@hotmail.com>
2024-01-18 14:09:44 +0000

    adding updated jsx and css files for About section

--- src/components/AboutUs/index.jsx
+++ src/components/AboutUs/index.jsx
@@ -1,11 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
+import React from 'react';
 
 function AboutUs() {
   return (
-    <div className="about-us-container">
-      <h1>About Us</h1>
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h2 className="display-4">About Us</h2>
+        <p className="lead">At QuiglyCare, we understand the importance of compassionate and reliable care for your 
+loved ones. With our comprehensive range of services, we provide a nurturing and 
+supportive environment that enhances their well-being. Explore the possibilities with 
+QuiglyCare and experience personalised care tailored to your unique needs.</p>
+      </div>
     </div>
   );
-}
+};
 
 export default AboutUs;

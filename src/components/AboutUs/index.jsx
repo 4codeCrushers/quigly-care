cfd916a261d732f5d667aa62e10ae679ee7a3d2c
Dom B <dominicbrown4@hotmail.com>
2024-01-18 20:45:48 +0000

    adding updated files with additional placeholder images

--- src/components/AboutUs/index.jsx
+++ src/components/AboutUs/index.jsx
@@ -5,13 +5,29 @@ import aboutUsData from '../../data/aboutUsData.json';
 
 function AboutUs() {
   return (
-    <div className="jumbotron jumbotron-fluid">
+    <div>
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <h2 className="display-4">{aboutUsData.title}</h2>
+          <p className="lead">{aboutUsData.content}</p>
+        </div>
+      </div>
+      
       <div className="container">
-        <h2 className="display-4">{aboutUsData.title}</h2>
-        <p className="lead">{aboutUsData.content}</p>
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://placehold.co/600x400" alt="Placeholder 1" className="img-fluid left-image" />
+          </div>
+          <div className="col-md-4">
+            <img src="https://placehold.co/600x400" alt="Placeholder 2" className="img-fluid middle-image" />
+          </div>
+          <div className="col-md-4">
+            <img src="https://placehold.co/600x400" alt="Placeholder 3" className="img-fluid right-image" />
+          </div>
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default AboutUs;

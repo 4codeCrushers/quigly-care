10a86676471f7e39855905380d52468adc1c745b
Dom B <dominicbrown4@hotmail.com>
2024-01-18 19:22:08 +0000

    adding updated index.jsx file and data file

--- src/components/AboutUs/index.jsx
+++ src/components/AboutUs/index.jsx
@@ -1,16 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import React from 'react';
+import aboutUsData from '../../data/aboutUsData.json';
 
 function AboutUs() {
   return (
     <div className="jumbotron jumbotron-fluid">
       <div className="container">
-        <h2 className="display-4">About Us</h2>
-        <p className="lead">At QuiglyCare, we understand the importance of compassionate and reliable care for your 
-loved ones. With our comprehensive range of services, we provide a nurturing and 
-supportive environment that enhances their well-being. Explore the possibilities with 
-QuiglyCare and experience personalised care tailored to your unique needs.</p>
+        <h2 className="display-4">{aboutUsData.title}</h2>
+        <p className="lead">{aboutUsData.content}</p>
       </div>
     </div>
   );

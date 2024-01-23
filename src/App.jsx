35c70bb05441734c32bf87156f4c38cdabd20629
Ewelina Sobora <e@e-dot.uk>
2024-01-23 20:38:16 +0000

    This is a combination of 2 commits.
    fix path to homePage page
    
    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css'
-import HomePage from './pages/homePage'
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (

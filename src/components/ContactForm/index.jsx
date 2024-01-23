1e3a02d8f562f7a5539ef17b2bc767e5f783802b
bonfield2l <lucy.bonfield2@paddypowerbetfair.com>
2024-01-23 20:11:36 +0000

    fixed input box layout in form

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -77,61 +77,60 @@ function ContactForm({ showForm, onClose }) {
     }
   };
 
-  return (
-    <div className={`contact-form-container ${showForm ? 'visible' : 'hidden'}`}>
-      <h1>Enquiries</h1>
-      <p>We would love to hear from you!</p>
-      <p>Please fill out the contact form below.</p>
-      <form action="https://api.web3forms.com/submit" method="POST" className="needs-validation" onSubmit={handleSubmit}>
-        <div className="form-row">
-          <div className="col-md-6 mb-3">
-            <label htmlFor="validationCustomFirstName">First name</label>
-            <input type="text" className={`form-control ${errors.firstName ? 'is-invalid' : ''}`}
-              id="validationCustomFirstName" name="firstName" placeholder="First Name" aria-describedby="inputGroupPrepend"
-              value={formData.firstName} onChange={handleInputChange}/>
-            <div className="invalid-feedback">{errors.firstName}</div>
-          </div>
-          <div className="col-md-6 mb-3">
-            <label htmlFor="validationCustomLastName">Last name</label>
-            <input type="text" className={`form-control ${errors.lastName ? 'is-invalid' : ''}`}
-              id="validationCustomLastName" name="lastName" placeholder="Last Name" aria-describedby="inputGroupPrepend"
-              value={formData.lastName} onChange={handleInputChange}/>
-            <div className="invalid-feedback">{errors.lastName}</div>
-          </div>
+return (
+  <div className={`contact-form-container ${showForm ? 'visible' : 'hidden'}`}>
+    <h1>Enquiries</h1>
+    <p>We would love to hear from you!</p>
+    <p>Please fill out the contact form below.</p>
+    <form action="https://api.web3forms.com/submit" method="POST" className="needs-validation" onSubmit={handleSubmit}>
+      <div className="form-group row">
+        <div className="col-md-6">
+          <label htmlFor="validationCustomFirstName">First name</label>
+          <input type="text" className={`form-control ${errors.firstName ? 'is-invalid' : ''}`}
+            id="validationCustomFirstName" name="firstName" placeholder="First Name"
+            value={formData.firstName} onChange={handleInputChange}/>
+          <div className="invalid-feedback">{errors.firstName}</div>
         </div>
-        <div className="form-row">
-          <div className="col-md-6 mb-3">
-            <label htmlFor="validationCustomEmail">Email</label>
-            <input type="email" className={`form-control ${errors.email ? 'is-invalid' : ''}`}
-              id="validationCustomEmail" name="email" placeholder="Email" aria-describedby="inputGroupPrepend"
-              value={formData.email} onChange={handleInputChange} autoComplete="email"/>
-            <div className="invalid-feedback">{errors.email}</div>
-          </div>
-          <div className="col-md-6 mb-3">
-            <label htmlFor="validationCustomSubject">Subject</label>
-            <input type="text" className={`form-control ${errors.subject ? 'is-invalid' : ''}`}
-              id="validationCustomSubject" name="subject" placeholder="Subject" aria-describedby="inputGroupPrepend"
-              value={formData.subject} onChange={handleInputChange}/>
-            <div className="invalid-feedback">{errors.subject}</div>
-          </div>
+        <div className="col-md-6">
+          <label htmlFor="validationCustomLastName">Last name</label>
+          <input type="text" className={`form-control ${errors.lastName ? 'is-invalid' : ''}`}
+            id="validationCustomLastName" name="lastName" placeholder="Last Name"
+            value={formData.lastName} onChange={handleInputChange}/>
+          <div className="invalid-feedback">{errors.lastName}</div>
         </div>
-        <div className="form-row">
-          <div className="col-md-12 mb-3">
-            <label htmlFor="validationCustomMessage">Message</label>
-            <textarea className={`form-control ${errors.message ? 'is-invalid' : ''}`}
-              id="validationCustomMessage" name="message" placeholder="Message"
-              rows="4"
-              value={formData.message} onChange={handleInputChange}></textarea>
-            <div className="invalid-feedback">{errors.message}</div>
-          </div>
+      </div>
+      <div className="form-group row">
+        <div className="col-md-6">
+          <label htmlFor="validationCustomEmail">Email</label>
+          <input type="email" className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+            id="validationCustomEmail" name="email" placeholder="Email"
+            value={formData.email} onChange={handleInputChange}
+            autoComplete="email"/>
+          <div className="invalid-feedback">{errors.email}</div>
+        </div>  
+        <div className="col-md-6">
+          <label htmlFor="validationCustomSubject">Subject</label>
+          <input type="text" className={`form-control ${errors.subject ? 'is-invalid' : ''}`}
+            id="validationCustomSubject" name="subject" placeholder="Subject"
+            value={formData.subject} onChange={handleInputChange}/>
+          <div className="invalid-feedback">{errors.subject}</div>
         </div>
-
-        <button className="btn btn-primary" type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+      </div>
+      <div className="form-group">
+        <label htmlFor="validationCustomMessage">Message</label>
+        <textarea className={`form-control ${errors.message ? 'is-invalid' : ''}`}
+          id="validationCustomMessage" name="message" placeholder="Message"
+          rows="4"
+          value={formData.message} onChange={handleInputChange}>
+        </textarea>
+        <div className="invalid-feedback">{errors.message}</div>
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  </div>
+);
 }
 
 export default ContactForm;

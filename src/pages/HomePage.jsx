91b032301d6785706f9a6994c714cd0ecd097318
Dom B <dominicbrown4@hotmail.com>
2024-01-23 21:19:39 +0000

    adding services section files and images

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,14 +3,16 @@ import AboutUs from '../components/AboutUs';
 import ContactForm from '../components/ContactForm';
 import NavBar from '../components/NavBar';
 import Team from '../components/Team';
+import Services from '../components/Services';
 
 function HomePage() {
   return (
     <Container>
       <NavBar />
       <div className="home-page">
-        <h1>Welcome to Quingly Care</h1>
+        <h1>Welcome to Quigly Care</h1>
         <AboutUs />
+        <Services />
         <Team />
         <ContactForm />
       </div>
